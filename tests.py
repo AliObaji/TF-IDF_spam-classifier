2476b6db3da71bedb16403874ea7ad1d97ed21fb
Ali Al-Obaji <ali@techhive.io>
2019-08-04 15:46:19 +0300

    First commit

---
+++ tests.py
@@ -0,0 +1,60 @@
+from sklearn.model_selection import train_test_split
+from helpers import review_messages, remove_stopWords, stem_words
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import confusion_matrix
+
+
+def plain(data):
+    data['text'] = data['text'].apply(review_messages).apply(remove_stopWords).apply(stem_words)
+    X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size = 0.1, random_state = 1)
+    
+    vectorizer = TfidfVectorizer()
+    model = build_svm(X_train, y_train, vectorizer)
+    
+    #Predict test results
+   
+    X_test = vectorizer.transform(X_test)
+    y_pred = model.predict(X_test)
+    
+    return confusion_matrix(y_test, y_pred)
+    
+    
+def medium_cleanup(data):
+    data['text'] = data['text'].apply(review_messages).apply(remove_stopWords)
+    X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size = 0.1, random_state = 1)
+    
+    vectorizer = TfidfVectorizer()
+    model = build_svm(X_train, y_train, vectorizer)
+    
+    #Predict test results
+    X_test = vectorizer.transform(X_test)
+    y_pred = model.predict(X_test)
+        
+    return confusion_matrix(y_test, y_pred)
+    
+    
+def high_cleanup(data):
+    data['text'] = data['text'].apply(review_messages).apply(remove_stopWords).apply(stem_words)
+    X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size = 0.1, random_state = 1)
+    
+    vectorizer = TfidfVectorizer()
+    model = build_svm(X_train, y_train, vectorizer)
+    
+    #Predict test results
+    X_test = vectorizer.transform(X_test)
+    y_pred = model.predict(X_test)
+        
+    return confusion_matrix(y_test, y_pred)
+    
+    
+    
+def build_svm(x_train, y_train, vectorizer):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn import svm
+
+    x_train = vectorizer.fit_transform(x_train)
+
+    svm = svm.SVC(C=1000)
+    svm.fit(x_train, y_train)
+    
+    return svm;
